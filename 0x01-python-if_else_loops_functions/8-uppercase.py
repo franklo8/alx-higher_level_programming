7548984bdd440e2a7cfd01d960b1a7bb5d0f7bdb
franklo8 <franklinejeh@gmail.com>
2022-12-06 11:52:50 +0000

    function that prints a string in uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,6 @@ def uppercase(str):
     str1 = ""
     for i in str:
         if ord(i) >= 97 and ord(i) < 123:
-            #'chr' is used to find the character from ASCII value
             str1 += chr(ord(i) - 32)
         else:
             str1 += i
